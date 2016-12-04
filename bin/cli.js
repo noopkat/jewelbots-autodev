4bfb136d06f1706bc9223b935b6123e557aa4ad0
Suz Hinton <suz.hinton@gmail.com>
2016-12-04 17:21:02 -0500

    add upload functionality

--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,20 @@
 #!/usr/bin/env node
 var fs = require('fs');
 var path = require('path');
+var parseArgs = require('minimist');
 var findPort = require('../lib/findPort');
 var compiler = require('../lib/compiler');
+var uploader = require('../lib/uploader');
 
-// hard coded sketch file and destination for now
-// var sketchfile = path.join(process.cwd(), 'sketches', 'blue.ino');
-// var buildDest = path.join(process.cwd(), 'dist');
+var args = (process.argv.slice(2));
+var argv = parseArgs(args, {});
+var userAction = argv._[0];
+
+handleInput(userAction);
+
+function showHelp() {
+  console.log('usage:\njewelbots-autodev compile\njewelbots-autodev upload\njewelbots-autodev help');
+};
 
 function setup(callback) {
   var configPath = path.join(process.cwd(), '.jbauto');
@@ -23,25 +31,62 @@ function setup(callback) {
       return callback(new Error('could not find .jbauto config file in this directory, please make one :)'));
     }
   });
-}
+} 
 
-function compileSketch(config) {
+function compileSketch(config, callback) {
   compiler.build(config, function(error) {
-    console.log('made it to the end', error);
+    return callback(error);
   });
 }
 
-function uploadSketch() {
+function uploadSketch(config, callback) {
   findPort(function(error, port) {
-    console.log('found jewelbots port?', port, error);
+    if (error) return console.log(error);
+    if (!port) return console.log(new Error('could not find a connected Jewelbot.'));
+    console.log('found jewelbot on port ' + port);
+    config.port = port;
+    uploader.upload(config, function(error) {
+      return callback(error);
+    });
   });
 }
 
-// test call
-setup(function(error, config) {
-  // see the output
-  console.log(config);
+function handleInput(action) {
+  switch (action) {
+    case 'compile': {
+      setup(function(error, config) {
+        if (error) return console.log(error);
+        console.log('compiling ' + config['sketch-file'] + '...');
+        compileSketch(config, function(error) {
+          if (error) return console.log(error);
+          console.log('done.');
+        });
+      });
+      break;
+    }
 
-  // compile?
-  compileSketch(config);
-});
+    case 'upload': {
+      setup(function(error, config) {
+        if (error) return console.log(error);
+        uploadSketch(config, function(error) {
+          if (error) return console.log(error);
+          console.log('done.');
+        });
+      });
+      break;
+    }
+
+    case 'help': {
+      showHelp();
+      process.exit();
+      break;
+    }
+
+    default: {
+      // Invalid or no argument specified, show help and exit with an error status
+      showHelp();
+      process.exit(9);
+      break;
+    }
+  }
+}
